e11ef481984edc7b0170b0abf2067ee76ef9fdcb
Nguyen Van Linh <linh.nguyenvan@ncc.asia>
2025-04-09 23:25:28 +0700

    Fix TypeScript errors in frontend

--- src/api/MyUserApi.tsx
+++ src/api/MyUserApi.tsx
@@ -1,11 +1,12 @@
 
+import { User } from "@/types";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useMutation, useQuery } from "react-query";
 import { toast } from "sonner";
-import { User } from "./types";
+
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
- 
+
 export const useGetMyUser = () => {
   const { getAccessTokenSilently } = useAuth0();
 
